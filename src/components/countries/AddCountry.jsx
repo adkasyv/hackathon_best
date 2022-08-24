d2b169998a17db5d2931f7ce0f91f24c760165eb
adiletzh <adiletzyrgalbekov71@gmail.com>
2022-08-24 19:31:13 +0600

    Add countries

--- src/components/countries/AddCountry.jsx
+++ src/components/countries/AddCountry.jsx
@@ -4,6 +4,7 @@ import { useCountries } from "../../contexts/CountryContextProvider";
 
 const AddCountry = () => {
   const { addCountry } = useCountries();
+  console.log("Add Country");
 
   const navigate = useNavigate();
 
